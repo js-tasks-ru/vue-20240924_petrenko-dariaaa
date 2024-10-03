75f2432a4f0606091ef79aab70e0c3fb21c24ac5
DariaaaP <barinova_f@list.ru>
2024-10-04 00:56:59 +0400

    added solution for counter

--- 02-basics-2/10-counter/CounterApp.js
+++ 02-basics-2/10-counter/CounterApp.js
@@ -1,9 +1,15 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue/dist/vue.esm-bundler.js'
 
 export default defineComponent({
   name: 'CounterApp',
 
-  setup() {},
+  setup() {
+    const result = ref(0)
+
+    return {
+      result,
+    }
+  },
 
   template: `
     <div class="counter">
@@ -11,15 +17,18 @@ export default defineComponent({
         class="button button--secondary"
         type="button"
         aria-label="Decrement"
-        disabled
+        :disabled="result <= 0"
+        @click="result -= 1"
       >➖</button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count">{{ result }}</span>
 
       <button
         class="button button--secondary"
         type="button"
         aria-label="Increment"
+        :disabled="result >= 5"
+        @click="result += 1"
       >➕</button>
     </div>
   `,
